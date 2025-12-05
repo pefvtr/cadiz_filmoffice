cc7e58cfe376568f83ef4e827bd1f41d94e2f236
pefvtr <pef.vtr@gmail.com>
2025-12-05 09:19:28 +0000

    Configuracion de Prettier en Astrojs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   },
