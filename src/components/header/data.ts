fe5dc9dc83b169f3ac240af2970d56be851c6466
pefvtr <pef.vtr@gmail.com>
2026-01-08 13:27:28 +0000

    Refactorizacion de codigo

---
+++ src/components/header/data.ts
@@ -0,0 +1,27 @@
+const phrases = [
+  {
+    id: 1,
+    text: "La ciudad que posa para la eternidad",
+  },
+  {
+    id: 2,
+    text: "Donde cada rincón cuenta una historia",
+  },
+  {
+    id: 3,
+    text: "Un decorado vivo de mil historias",
+  },
+
+  {
+    id: 4,
+    text: "El alma de Cádiz, en cada fotograma",
+  },
+];
+
+const menuItems = [
+  { label: "Inicio", url: "/" },
+  { label: "¿Quiénes Somos?", url: "/quienes-somos" },
+  { label: "Contacto", url: "/contacto" },
+];
+
+export { phrases, menuItems };
