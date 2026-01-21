0f3c1e6a4d909b1640fdc9874e736f4427327c0f
pefvtr <pef.vtr@gmail.com>
2026-01-21 09:02:07 +0000

    Ajustes en la animacion del Slider de la pagina de inicio

---
+++ src/components/slider/data.ts
@@ -0,0 +1,52 @@
+const dataCarousel = [
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1659733582156-d2a11801e59f?q=50&w=1600",
+    title: "We're No",
+    subtitle: "Strangers to love",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1543362137-5df0547b039d?q=50&w=1600",
+    title: "You Know",
+    subtitle: "The rules and so do I",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1631142260228-305ccb610dba?q=50&w=1600",
+    title: "Full commitment",
+    subtitle: "Is what I'm thinking of",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1708022766976-49ca46c0f7de?q=50&w=1600",
+    title: "You Wouldn't",
+    subtitle: "Get this from any other guy",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1631142260079-970258649676?q=50&w=1600",
+    title: "I Just Wanna",
+    subtitle: "Tell you how I'm feeling",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1708022809820-2668e65877b9?q=50&w=1600",
+    title: "Gotta Make",
+    subtitle: "You understand",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1708022796522-ff65b57439de?q=50&w=1600",
+    title: "Never Gonna",
+    subtitle: "Give you up",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1708022790103-a514cb89a034?q=50&w=1600",
+    title: "Never Gonna",
+    subtitle: "Let you down",
+  },
+];
+
+export { dataCarousel };
